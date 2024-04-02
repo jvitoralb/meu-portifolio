b670fac6d4ac1c66b62150721fb635532b9bfe78
jvitoralb <jvvalb@gmail.com>
2024-04-02 16:29:10 -0300

    feat: create a custom dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,44 @@ toggleNavbarInputButton.addEventListener('click', (e) => {
     navbarClassList.toggle('navbar-open');
     document.body.classList.toggle('hidden-overflow');
 });
+
+const langsDropdown = document.querySelector('#langs-dropdown-btn');
+const langsList = document.querySelector('#langs-dropdown-list');
+
+
+function dropdownHandler() {
+    const langsListClassList = langsList.classList;
+    const langsItems = langsList.children;
+
+    function closeDropdownByDocument(e) {
+        if (langsList.classList.contains('open-dropdown') && ![...langsItems, langsDropdown].includes(e.target)) {
+            closeDropdown();
+        }
+    }
+    function langsItemsClickEvent(e) {
+        langsDropdown.textContent = e.target.textContent;
+        closeDropdown();
+    }
+    function closeDropdown() {
+        langsListClassList.remove('open-dropdown');
+        for(let i = 0; i < langsItems.length; i++) {
+            langsItems[i].removeEventListener('click', langsItemsClickEvent);
+        }
+        document.removeEventListener('click', closeDropdownByDocument);
+    }
+    function openDropdown() {
+        langsListClassList.add('open-dropdown');
+        for(let i = 0; i < langsItems.length; i++) {
+            langsItems[i].addEventListener('click', langsItemsClickEvent);
+        }
+        document.addEventListener('click', closeDropdownByDocument);
+    }
+    
+    if (langsListClassList.contains('open-dropdown')) {
+        closeDropdown();
+    } else {
+        openDropdown();
+    }
+}
+
+langsDropdown.addEventListener('click', dropdownHandler);
